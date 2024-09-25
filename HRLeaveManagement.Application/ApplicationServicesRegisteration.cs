8d8cb8d527f88337ac30d9a85b411ba402273e65
Raed Salah Bzour <raedbzour77@gmail.com>
2024-09-25 03:30:33 +0300

    adding domain and application

---
+++ HRLeaveManagement.Application/ApplicationServicesRegisteration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HRLeaveManagement.Application
+{
+    public static class ApplicationServicesRegisteration
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            //or this for specific file
+            //services.AddAutoMapper(typeof(MappingProfile));
+        }
+    }
+}
